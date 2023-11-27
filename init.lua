2e26228c30a9be5c3e67860e2eb8d580781105c2
Adinda Praditya <apraditya@gmail.com>
2023-11-27 11:56:40 +0700

    Move vim-matchup setting

--- init.lua
+++ init.lua
@@ -72,9 +72,6 @@ return {
     },
   },
 
-
-    "andymass/vim-matchup",
-
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -4,12 +4,18 @@ return {
     "nvim-treesitter/nvim-treesitter-textobjects",
   },
   {
+    "andymass/vim-matchup",
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         "lua",
       })
+      opts.matchup = {
+        enable = true,
+      }
       opts.textobjects = {
         select = {
           enable = true,
